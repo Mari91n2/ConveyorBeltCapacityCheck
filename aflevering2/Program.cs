37a5f1923670c83f6530b478c57e953165a53cef
Mari91n2 <mariamceesay05@gmail.com>
2025-11-17 13:27:13 +0100

    Update Program.cs

--- aflevering2/Program.cs
+++ aflevering2/Program.cs
@@ -1,14 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
 Console.WriteLine("How many motors are carrying the packages?");
 var antalmotor= Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("How many kg of packages do we expect?");
 var antalkg = Convert.ToDouble(Console.ReadLine());
 
-if(antalkg/antalmotor>=5.6) 
-    Console.WriteLine("Yes");
+if(antalkg/antalmotor<=5.6) 
+    Console.WriteLine("Yes! The conveyor belt can carry the packages.");
 else
 {
-    Console.WriteLine("No");
+    Console.WriteLine("No. The conveyor belt cannot carry the packages.");
 }
-
